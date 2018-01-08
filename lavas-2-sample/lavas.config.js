52c75d87361ea2afe632bc9d8cad71935d5fa294
Jouryjc <494208562@qq.com>
2018-01-08 23:18:21 +0800

    init

---
+++ lavas-2-sample/lavas.config.js
@@ -0,0 +1,49 @@
+/**
+ * @file lavas config
+ * @author Jouryjc(494208562@qq.com)
+ */
+
+'use strict';
+
+const path = require('path');
+const BUILD_PATH = path.resolve(__dirname, 'dist');
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+    build: {
+        ssr: true,
+        path: BUILD_PATH,
+        publicPath: '/',
+        ssrCopy: isDev ? [] : [
+            {
+                src: 'server.prod.js'
+            },
+            {
+                src: 'package.json'
+            }
+        ]
+    },
+    router: {
+        mode: 'history',
+        base: '/',
+        pageTransition: {
+            type: 'fade',
+            transitionClass: 'fade'
+        }
+    },
+    serviceWorker: {
+        swSrc: path.join(__dirname, 'core/service-worker.js'),
+        swDest: path.join(BUILD_PATH, 'service-worker.js'),
+        globDirectory: BUILD_PATH,
+        globPatterns: [
+            '**/*.{html,js,css,eot,svg,ttf,woff}'
+        ],
+        globIgnores: [
+            'sw-register.js',
+            '**/*.map'
+        ],
+        appshellUrl: '/appshell',
+        dontCacheBustUrlsMatching: /\.\w{8}\./
+    }
+};
